de4754d94a9a57d7ac892ddac53f06fa88bec94c
Alexis King <lexi.lambda@gmail.com>
2015-10-25 10:50:36 -0700

    Add file-error? and read-error?

--- base.rkt
+++ base.rkt
@@ -20,7 +20,7 @@
   6:cons 6:current-error-port 6:current-input-port 6:current-output-port 6:define define-record-type
   6:define-syntax r:define-values 6:denominator 5:display 6:do 6:dynamic-wind 6:else 6:eof-object
   6:eof-object? 6:eq? 6:equal? 6:eqv? error error-object-irritants error-object-message error-object?
-  6:even? 6:exact 6:exact-integer-sqrt r:exact-integer? 6:exact? 6:expt features file-error? 6:floor
+  6:even? 6:exact 6:exact-integer-sqrt r:exact-integer? 6:exact? 6:expt features 6:floor
   floor-quotient floor-remainder floor/ 6:flush-output-port 6:for-each 6:gcd get-output-bytevector
   get-output-string 6:guard 6:if include 6:inexact input-port-open? 6:input-port? 6:integer->char
   6:integer? 6:lambda 6:lcm 6:length 6:let 6:let* 6:let*-values 6:let-syntax 6:let-values 6:letrec
@@ -30,21 +30,20 @@
   6:number? 6:numerator 6:odd? open-input-bytevector open-input-string open-output-bytevector
   open-output-string 6:or output-port-open? 6:pair? r:parameterize 5:peek-char peek-u8 6:port?
   6:positive? 6:procedure? 6:quasiquote 6:quote 5:quotient 6:raise 6:raise-continuable 6:rational?
-  6:rationalize read-bytevector read-bytevector! 5:read-char read-error? r:read-line r:read-string
-  read-u8 6:real? 5:remainder 6:reverse 6:round 6:set! 5:set-car! 5:set-cdr! square 6:string
-  6:string->list 6:string->number 6:string->symbol string->vector 6:string-append 6:string-copy
-  r:string-copy! 5:string-fill! 6:string-for-each 6:string-length string-map 6:string-ref
-  5:string-set! 6:string<=? 6:string<? 6:string=? 6:string>=? 6:string>? 6:string? 6:substring
-  6:symbol->string 6:symbol=? 6:symbol? syntax-error 6:textual-port? 6:truncate truncate-quotient
-  truncate-remainder truncate/ u8-ready? 6:unless 6:unquote 6:unquote-splicing 6:values 6:vector
-  6:vector->list vector->string r:vector-append r:vector-copy r:vector-copy! 6:vector-fill!
-  6:vector-for-each 6:vector-length 6:vector-map 6:vector-ref 6:vector-set! 6:vector 6:when
-  6:with-exception-handler write-bytevector 5:write-char write-string write-u8 6:zero?)
- (rename-out [string->bytes/utf-8 string->utf8]
+  6:rationalize read-bytevector read-bytevector! 5:read-char r:read-line r:read-string read-u8 6:real?
+  5:remainder 6:reverse 6:round 6:set! 5:set-car! 5:set-cdr! square 6:string 6:string->list
+  6:string->number 6:string->symbol string->vector 6:string-append 6:string-copy r:string-copy!
+  5:string-fill! 6:string-for-each 6:string-length string-map 6:string-ref 5:string-set! 6:string<=?
+  6:string<? 6:string=? 6:string>=? 6:string>? 6:string? 6:substring 6:symbol->string 6:symbol=?
+  6:symbol? syntax-error 6:textual-port? 6:truncate truncate-quotient truncate-remainder truncate/
+  u8-ready? 6:unless 6:unquote 6:unquote-splicing 6:values 6:vector 6:vector->list vector->string
+  r:vector-append r:vector-copy r:vector-copy! 6:vector-fill! 6:vector-for-each 6:vector-length
+  6:vector-map 6:vector-ref 6:vector-set! 6:vector 6:when 6:with-exception-handler write-bytevector
+  5:write-char write-string write-u8 6:zero?)
+ (rename-out [exn:fail:filesystem? file-error?]
+             [exn:fail:read? read-error?]
+             [string->bytes/utf-8 string->utf8]
              [bytes->string/utf-8 utf8->string]))
-
-(define-syntax-rule (file-error? . _)
-  (syntax-error "FIXME: not yet implemented"))
 
 (define-syntax-rule (floor-quotient . _)
   (syntax-error "FIXME: not yet implemented"))
@@ -103,9 +102,6 @@
   (syntax-error "FIXME: not yet implemented"))
 
 (define-syntax-rule (read-bytevector! . _)
-  (syntax-error "FIXME: not yet implemented"))
-
-(define-syntax-rule (read-error? . _)
   (syntax-error "FIXME: not yet implemented"))
 
 (define-syntax-rule (read-u8 . _)
