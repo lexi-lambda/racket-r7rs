0daeea007cf0915de5e22dd6a7569c8b4786779c
Philip E Blair <peblairman@gmail.com>
2015-10-25 20:53:45 -0400

    Added bytevector to readtable and made identifiers case-sensitive (as per R7RS Sec. 2.1)

--- load/lang/reader.rkt
+++ load/lang/reader.rkt
@@ -1,8 +1,10 @@
 #lang s-exp syntax/module-reader r7rs/load/lang/language
 
 #:wrapper1 (lambda (t)
-             (parameterize ([read-case-sensitive #f]
+             (parameterize ([current-readtable (r7rs:make-r7rs-readtable)]
                             [read-accept-infix-dot #f]
                             [read-curly-brace-as-paren #f]
                             [read-square-bracket-as-paren #f])
                (t)))
+
+(require (prefix-in r7rs: "../../lang/r7rs-reader.rkt"))
