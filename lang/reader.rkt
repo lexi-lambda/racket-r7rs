38362f9eea5552a7adf7892c52dcfcd4f48b3119
Philip E Blair <peblairman@gmail.com>
2015-10-25 21:41:07 -0400

    Fixed EOF newlines and removed unneeded 7rs: prefix

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -1,10 +1,10 @@
 #lang s-exp syntax/module-reader r7rs
 
 #:wrapper1 (lambda (t)
-             (parameterize ([current-readtable (r7rs:make-r7rs-readtable)]
+             (parameterize ([current-readtable (make-r7rs-readtable)]
                             [read-accept-infix-dot #f]
                             [read-curly-brace-as-paren #f]
                             [read-square-bracket-as-paren #f])
                (t)))
 
-(require (prefix-in r7rs: "r7rs-reader.rkt"))
+(require "r7rs-reader.rkt")
