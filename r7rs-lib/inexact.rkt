c718a39a37931c36e728e80f471092ea7a42f5d3
Alexis King <lexi.lambda@gmail.com>
2015-10-31 18:13:34 -0700

    Allow infinite?, finite?, and nan? to operate on complex arguments

--- r7rs-lib/inexact.rkt
+++ r7rs-lib/inexact.rkt
@@ -1,7 +1,23 @@
 #lang racket/base
 
-(require (prefix-in 6: rnrs/base-6)
+(require racket/contract
+         (prefix-in 6: rnrs/base-6)
+         (prefix-in r: racket/math)
          "private/strip-prefix.rkt")
 
-(provide (strip-colon-prefix-out 6:acos 6:asin 6:atan 6:cos 6:exp 6:finite?
-                                 6:infinite? 6:log 6:nan? 6:sin 6:sqrt 6:tan))
+(provide infinite? finite? nan?
+         (strip-colon-prefix-out 6:acos 6:asin 6:atan 6:cos 6:exp 6:log 6:sin 6:sqrt 6:tan))
+
+(define/contract (infinite? x)
+  (number? . -> . boolean?)
+  (or (r:infinite? (real-part x))
+      (r:infinite? (imag-part x))))
+
+(define/contract (finite? x)
+  (number? . -> . boolean?)
+  (not (infinite? x)))
+
+(define/contract (nan? x)
+  (number? . -> . boolean?)
+  (or (r:nan? (real-part x))
+      (r:nan? (imag-part x))))
