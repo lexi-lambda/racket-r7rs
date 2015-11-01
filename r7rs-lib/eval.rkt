65641c2b7deca46535cf3dc9f6e12b1b3f045288
Alexis King <lexi.lambda@gmail.com>
2015-11-01 10:46:07 -0800

    Re-implement environment and eval for R7RS

--- r7rs-lib/eval.rkt
+++ r7rs-lib/eval.rkt
@@ -1,6 +1,26 @@
 #lang racket/base
 
-(require (prefix-in 6: rnrs/eval-6)
-         "private/strip-prefix.rkt")
+(require (prefix-in r: racket/base))
 
-(provide (strip-colon-prefix-out 6:environment 6:eval))
+(provide environment eval)
+
+(define-namespace-anchor anchor)
+
+(define (to-immutable p)
+  (cond
+    [(mpair? p) (cons (to-immutable (mcar p))
+                      (to-immutable (mcdr p)))]
+    [(vector? p)
+     (let ([v (list->vector (map to-immutable (vector->list p)))])
+       (if (immutable? p) (vector->immutable-vector v) v))]
+    [else p]))
+
+(define (environment . import-specs)
+  (let ([ns (namespace-anchor->empty-namespace anchor)])
+    (parameterize ([current-namespace ns])
+      (namespace-require 'r7rs)
+      (r:eval `(import . ,(map to-immutable import-specs))))
+    ns))
+
+(define (eval expr env)
+  (r:eval (to-immutable expr) env))
