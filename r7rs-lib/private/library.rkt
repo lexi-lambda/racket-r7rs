be6df1b667df5d9392661dcb05b1f606cdbcb0ec
Alexis King <lexi.lambda@gmail.com>
2016-06-12 00:22:11 -0700

    Restrict the clauses that may appear directly within define-library
    Fixes #5

--- r7rs-lib/private/library.rkt
+++ r7rs-lib/private/library.rkt
@@ -1,13 +1,33 @@
 #lang racket/base
 
 (require (for-syntax racket/base
-                     syntax/parse))
+                     syntax/parse)
+         racket/include
+         "cond-expand.rkt"
+         "export.rkt"
+         "import.rkt")
 
 (provide define-library)
+
+; Because define-library forms use import/export/begin/cond-expand/include identifiers but are likely
+; expanded in a context where those identifiers are not in scope (at least for
+; begin/cond-expand/include), the library-clause syntax class adds a layer of indirection that matches
+; each possible clause by datum instead of by identifier, then expands to the identifier with proper
+; lexical context.
+
+(begin-for-syntax
+  (define-syntax-class library-clause
+    #:attributes [with-ctx]
+    [pattern ({~datum import} . body) #:attr with-ctx #'(import . body)]
+    [pattern ({~datum export} . body) #:attr with-ctx #'(export . body)]
+    [pattern ({~datum begin} . body) #:attr with-ctx #'(begin . body)]
+    [pattern ({~datum cond-expand} . body) #:attr with-ctx #'(cond-expand . body)]
+    [pattern ({~datum include} . body)
+             #:attr with-ctx #`(include-at/relative-to #,this-syntax #,this-syntax . body)]))
 
 (define-syntax (define-library stx)
   (unless (eq? (syntax-local-context) 'module)
     (raise-syntax-error 'define-library "not in module context" stx))
   (syntax-parse stx
-    [(_ _ body ...)
-     #'(begin body ...)]))
+    [(_ _ clause:library-clause ...)
+     #'(begin clause.with-ctx ...)]))
